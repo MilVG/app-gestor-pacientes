b02ae489b656d18217e7ef8fd7c1742896153330
notlim_dev <elmotigre6@gmail.com>
2024-09-23 00:24:42 -0500

    0002: Incorporación de estructura de Diseño General.
    se Agregó los componentes a y se definió la estructura de la aplicacion.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import PacientesForm from "./components/PacientesForm"
+import PacientesList from "./components/PacientesList"
 
 function App() {
 
   return (
-    <div className="bg-sky-300">
-      Pacientes
+    <div className="container bg-sky-300 mx-auto">
+      <h1 className="text-5xl font-black text-center md:w-2/3 md:mx-auto"
+      > Seguimiento de Pacientes <span className="text-indigo-700">Veterinaria</span>
+      </h1>
+      <div className="bg-orange-300 mt-12 md:flex">
+        <PacientesForm />
+        <PacientesList />
+
+      </div>
     </div>
   )
 }
